bf7b41cf4cd71ee694d30d9da321fd325bb47357
sarma-akondi <63581910+sarma-akondi@users.noreply.github.com>
2020-09-09 19:36:16 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from "./Header"
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+   render() {
+      return (
+         <div>
+            <Header />
+            <button>Add</button>
+            <div>
+               <span>Name</span><br />
+               <span>Phone</span>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = function () {
+    return (
+        <div className="header">Phone Directory</div>
+    )
+}
+
+export default Header;
