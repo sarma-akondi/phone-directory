cf192e5820cea2ac1eb82b594a9b28cfa901d301
sarma-akondi <63581910+sarma-akondi@users.noreply.github.com>
2020-09-13 15:58:15 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import Header from "./Header"
+import Header from "./Header";
+import './App.css';
 
 class App extends Component {
+
    render() {
+
+      let subscribers = [
+         {
+            id: 1,
+            name: 'Sarma',
+            phone: '111111111111'
+         },
+         {
+            id: 2,
+            name: 'Dharma',
+            phone: '2222222222'
+         }
+      ]
+
       return (
          <div>
             <Header />
-            <button>Add</button>
-            <div>
-               <span>Name</span><br />
-               <span>Phone</span>
+            <div className="component-body-container">
+               <button className="custom-btn add-btn">Add</button>
+
+               <div className="grid-container heading-container">
+                  <span className="grid-item name-heading">Name</span>
+                  <span className="grid-item phone-heading">Phone</span>
+               </div>
+
+               {
+                  subscribers.map(sub => {
+                     return (
+                        <div key={sub.id} className="grid-container">
+                           <span className="grid-item">{sub.name}</span>
+                           <span className="grid-item">{sub.phone}</span>
+                        </div>
+                     )
+                  })
+               }
+
             </div>
          </div>
       );
