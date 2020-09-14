2aa47b24700fd067d973b6fda6c2dead171c200a
sarma-akondi <63581910+sarma-akondi@users.noreply.github.com>
2020-09-14 14:38:27 +0530

    Added routing for Add Subscriber page and Show Subscribers page

--- src/PhoneDirectory.js
+++ src/PhoneDirectory.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AddSubscriber from './AddSubscriber';
 import ShowSubscribers from './ShowSubscribers';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class PhoneDirectory extends Component {
 
@@ -22,7 +23,7 @@ class PhoneDirectory extends Component {
 
     addSubscriberHandler = (newSubscriber) => {
         let subscribersList = this.state.subscribersList;
-        if(subscribersList.length > 0){
+        if (subscribersList.length > 0) {
             newSubscriber.id = subscribersList[subscribersList.length - 1].id + 1;
         }
         else {
@@ -30,13 +31,17 @@ class PhoneDirectory extends Component {
         }
 
         subscribersList.push(newSubscriber);
-        this.setState({subscribersList: subscribersList});
+        this.setState({ subscribersList: subscribersList });
     }
 
     render() {
         return (
-            // <AddSubscriber addSubscriberHandler={this.addSubscriberHandler}/>
-            <ShowSubscribers subscribersList={this.state.subscribersList}/>
+            <Router>
+                <div className="main-container">
+                    <Route exact path='/' render={(props) => <ShowSubscribers {...props} subscribersList={this.state.subscribersList} />} />
+                    <Route exact path='/add' render={({history},props) => <AddSubscriber history={history} {...props} addSubscriberHandler={this.addSubscriberHandler} />} />
+                </div>
+            </Router>
         )
     }
 }
