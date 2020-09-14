172c3da03c8950c37e746c9528246cde80838da7
sarma-akondi <63581910+sarma-akondi@users.noreply.github.com>
2020-09-14 13:15:13 +0530

    Added ‘PhoneDirectory’ as smart component, renamed ‘App’ as ‘ShowSubscribers’ and converted ‘AddSubscribers’ and ‘ShowSubscribers’ as dumb components

---
+++ src/ShowSubscribers.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Header from "./Header";
+import './ShowSubscribers.css';
+
+
+// let subscribers = [
+//    {
+//       id: 1,
+//       name: 'Sarma',
+//       phone: '111111111111'
+//    },
+//    {
+//       id: 2,
+//       name: 'Dharma',
+//       phone: '2222222222'
+//    }
+// ]
+
+
+class ShowSubscribers extends Component {
+
+   render() {
+      return (
+         <div>
+            <Header heading="Phone Directory" />
+            <div className="component-body-container">
+               <button className="custom-btn add-btn">Add</button>
+
+               <div className="grid-container heading-container">
+                  <span className="grid-item name-heading">Name</span>
+                  <span className="grid-item phone-heading">Phone</span>
+               </div>
+
+               {
+                  this.props.subscribersList.map(sub => {
+                     return (
+                        <div key={sub.id} className="grid-container">
+                           <span className="grid-item">{sub.name}</span>
+                           <span className="grid-item">{sub.phone}</span>
+                           <span className="grid-item action-btn-container">
+                              <button className="custom-btn delete-btn">Delete</button>
+                           </span>
+                        </div>
+                     )
+                  })
+               }
+
+            </div>
+         </div>
+      );
+   }
+}
+
+export default ShowSubscribers;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ShowSubscribers from './ShowSubscribers';
 import * as serviceWorker from './serviceWorker';
 import AddSubscriber from './AddSubscriber';
 import './common/common.css'
+import PhoneDirectory from './PhoneDirectory';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<PhoneDirectory />,document.getElementById('root'));
 serviceWorker.unregister();
